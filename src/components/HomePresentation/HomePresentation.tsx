8cfcda73cbd71f10558787867c8a2807978a07ab
Marcio Flavio <mflaviof1995@gmail.com>
2022-08-15 10:04:21 -0300

    feat: Mobile adaptation

--- src/components/HomePresentation/HomePresentation.tsx
+++ src/components/HomePresentation/HomePresentation.tsx
@@ -3,9 +3,9 @@ import styles from "./HomePresentation.module.css";
 const HomePresentation = () => {
   return (
     <div className={styles.container}>
-      <div style={{ width: "750px", margin: "0 auto" }}>
-        <h1>Travel Time</h1>
-        <span>
+      <div className={styles.text_container}>
+        <h1 className={styles.title}>Travel Time</h1>
+        <span className={styles.text}>
           let's travel with us we will start a new adventure together with a
           full of happiness and laughs
         </span>
